79a73636a21bcbf54b0474892161389169ecc81f
NaicikOne <NaicikOne@yandex.ru>
2021-04-21 23:41:11 +0600

    Проблемы с именем бота, и выбор бота может превышать кол-во спичек

--- gamePVE.c
+++ gamePVE.c
@@ -1,4 +1,75 @@
-#include "gamePVP.h"
+#include "gamePVE.h"
 #include "out.h"
 
-int gamePVE(char *player_one) { return 0; }
+int gamePVE(char *player_one, char *player_two) {
+  nickname_out(player_one);
+  printf("Player's nickname(no more than 15 characters): ");
+  scanf("%s", player_one);
+  void choise_dif();
+  int choise;
+  printf("1 - easy. 2-medium. 3-hard.\n");
+  scanf("%d", &choise);
+  int winner = 0;
+  int input = 0, stick = 100;
+  int num[3];
+  num[0] = input;
+  num[1] = stick;
+  num[2] = winner;
+  if (choise == 1)
+    winner = bot_easy(num);
+  if (choise == 2)
+    winner = bot_medium(num);
+  if (choise == 3)
+    winner = bot_hard(num);
+  return winner;
+}
+
+int bot_easy(int *num) {
+  srand(time(NULL));
+  int choise = 0;
+  while (num[0] != 100) {
+    game(num[1]);
+    printf("\n\n");
+    printf("Player, enter the number: ");
+    scanf("%d", &choise);
+    if (choise > num[1]) {
+      printf("\nThere are too few matches, try again");
+      choise = 100;
+    }
+
+    while (choise < 1 || choise > 10) {
+      choise = 0;
+      printf("\nThe number must be at least 1, and not more than 10");
+      printf("\nPlayer, enter the number: ");
+      scanf("%d", &choise);
+      if (choise > num[1]) {
+        printf("\nThere are too few matches, try again");
+        choise = 100;
+      }
+    }
+    *num += choise;
+    *(num + 1) -= choise;
+    num[2] = 2;
+
+    game(num[1]);
+    printf("\n\n");
+    choise = rand() % 10 + 1;
+    *num += choise;
+    *(num + 1) -= choise;
+    num[2] = 1;
+  }
+  int winner = num[2];
+  return winner;
+}
+
+int bot_medium(int *num) {
+  printf("It is under revision");
+  system("pause");
+  return 0;
+}
+
+int bot_hard(int *num) {
+  printf("It is under revision");
+  system("pause");
+  return 0;
+}
--- gamePVE.h
+++ gamePVE.h
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #pragma once
 
-int gamePVE(char *);
+int gamePVE(char *, char *);
+int bot_easy(int *);
+int bot_medium(int *);
+int bot_hard(int *);
