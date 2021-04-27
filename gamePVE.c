22b6e999352598b69034e66abdb9e6ab81e3050b
NaicikOne <NaicikOne@yandex.ru>
2021-04-27 18:49:14 +0600

    unnecessary variable removed

--- gamePVE.c
+++ gamePVE.c
@@ -2,37 +2,32 @@
 #include "out.h"
 
 int gamePVE(char *player_one) {
-  nickname_out(player_one);
-  printf("Player's nickname(no more than 15 characters): ");
-  scanf("%s", player_one);
-  choise_dif();
+  int i = 1;
+  nickname_out(player_one, i);
   int choise;
-  printf("1 - easy. 2-medium. 3-hard.\n");
-  scanf("%d", &choise);
-  int winner = 0;
-  int input = 0, stick = 100;
+  choise = choise_dif();
   int num[3];
-  num[0] = input;
-  num[1] = stick;
-  num[2] = winner;
+  num[0] = 100;
+  // stick
+  num[1] = 0;
+  // winner
   if (choise == 1)
-    winner = bot_easy(num);
+    num[1] = bot_easy(num);
   if (choise == 2)
-    winner = bot_medium(num);
+    num[1] = bot_medium(num);
   if (choise == 3)
-    winner = bot_hard(num);
-  return winner;
+    num[1] = bot_hard(num);
+  return num[1];
 }
 
 int bot_easy(int *num) {
   srand(time(NULL));
   int choise = 0;
-  while (num[0] != 100) {
-    game(num[1]);
-    printf("\n\n");
+  while (num[0] != 0) {
+    game(num[0]);
     printf("Player, enter the number: ");
     scanf("%d", &choise);
-    if (choise > num[1]) {
+    if (choise > num[0]) {
       printf("\nThere are too few matches, try again");
       choise = 100;
     }
@@ -42,38 +37,35 @@ int bot_easy(int *num) {
       printf("\nThe number must be at least 1, and not more than 10");
       printf("\nPlayer, enter the number: ");
       scanf("%d", &choise);
-      if (choise > num[1]) {
+      if (choise > num[0]) {
         printf("\nThere are too few matches, try again");
         choise = 100;
       }
     }
-    *num += choise;
-    *(num + 1) -= choise;
-    num[2] = 2;
+    *num -= choise;
+    num[1] = 2;
 
-    if (num[1] != 0) {
-      if (num[1] <= 10)
-        choise = num[1];
+    if (num[0] != 0) {
+      if (num[0] <= 10)
+        choise = num[0];
       else
         choise = rand() % 10 + 1;
-      *num += choise;
-      *(num + 1) -= choise;
-      num[2] = 1;
+      *num -= choise;
+      num[1] = 1;
     }
   }
-  int winner = num[2];
+  int winner = num[1];
   return winner;
 }
 
 int bot_medium(int *num) {
   srand(time(NULL));
   int choise = 0;
-  while (num[0] != 100) {
-    game(num[1]);
-    printf("\n\n");
+  while (num[0] != 0) {
+    game(num[0]);
     printf("Player, enter the number: ");
     scanf("%d", &choise);
-    if (choise > num[1]) {
+    if (choise > num[0]) {
       printf("\nThere are too few matches, try again");
       choise = 100;
     }
@@ -83,40 +75,38 @@ int bot_medium(int *num) {
       printf("\nThe number must be at least 1, and not more than 10");
       printf("\nPlayer, enter the number: ");
       scanf("%d", &choise);
-      if (choise > num[1]) {
+      if (choise > num[0]) {
         printf("\nThere are too few matches, try again");
         choise = 100;
       }
     }
-    *num += choise;
-    *(num + 1) -= choise;
-    num[2] = 2;
+    *num -= choise;
+    num[1] = 2;
 
-    if (num[1] != 0) {
-      if (num[1] <= 10)
-        choise = num[1] - 1;
-      if (num[1] == 1)
+    if (num[0] != 0) {
+      if (num[0] <= 10)
+        choise = num[0] - 1;
+      if (num[0] == 1)
         choise = 1;
       else
         choise = rand() % 10 + 1;
-      *num += choise;
-      *(num + 1) -= choise;
-      num[2] = 1;
+
+      *num -= choise;
+      num[1] = 1;
     }
   }
-  int winner = num[2];
+  int winner = num[1];
   return winner;
 }
 
 int bot_hard(int *num) {
   srand(time(NULL));
   int choise = 0;
-  while (num[0] != 100) {
-    game(num[1]);
-    printf("\n\n");
+  while (num[0] != 0) {
+    game(num[0]);
     printf("Player, enter the number: ");
     scanf("%d", &choise);
-    if (choise > num[1]) {
+    if (choise > num[0]) {
       printf("\nThere are too few matches, try again");
       choise = 100;
     }
@@ -126,26 +116,24 @@ int bot_hard(int *num) {
       printf("\nThe number must be at least 1, and not more than 10");
       printf("\nPlayer, enter the number: ");
       scanf("%d", &choise);
-      if (choise > num[1]) {
+      if (choise > num[0]) {
         printf("\nThere are too few matches, try again");
         choise = 100;
       }
     }
-    *num += choise;
-    *(num + 1) -= choise;
-    num[2] = 2;
+    *num -= choise;
+    num[1] = 2;
 
-    if (num[1] != 0) {
-      if (num[1] <= 10)
-        choise = num[1] - 1;
-      if (num[1] == 1)
+    if (num[0] != 0) {
+      if (num[0] <= 10)
+        choise = num[0] - 1;
+      if (num[0] == 1)
         choise = 1;
-      else
-        *num += choise;
-      *(num + 1) -= choise;
-      num[2] = 1;
+
+      *num -= choise;
+      num[1] = 1;
     }
   }
-  int winner = num[2];
+  int winner = num[1];
   return winner;
 }
--- gamePvP.c
+++ gamePvP.c
@@ -2,31 +2,26 @@
 #include "out.h"
 
 int gamePVP(char *player_one, char *player_two) {
-  nickname_out();
-  printf("Nickname of the first player(no more than 15 characters): ");
-  scanf("%s", player_one);
-  printf("Nickname of the second player(no more than 15 characters): ");
-  scanf("%s", player_two);
-  printf("\n");
-  int input = 0, stick = 100;
-  int num[3];
-  num[0] = input;
-  num[1] = stick;
-  int winner = 0;
-  num[2] = winner;
-  winner = maingame(num);
-  return winner;
+  int i = 2;
+  nickname_out(player_one, i);
+  i++;
+  nickname_out(player_two, i);
+  int num[2];
+  num[0] = 100;
+  // stick
+  num[1] = 0;
+  // winner
+  num[1] = maingame(num);
+  return num[1];
 }
 
 int maingame(int *num) {
   int choise = 0;
-  while (num[0] != 100) {
-    game(num[1]);
-    printf("\n\n");
-
+  while (num[0] != 0) {
+    game(num[0]);
     printf("Player #1, enter the number: ");
     scanf("%d", &choise);
-    if (choise > num[1]) {
+    if (choise > num[0]) {
       printf("\nThere are too few matches, try again");
       choise = 100;
     }
@@ -36,21 +31,18 @@ int maingame(int *num) {
       printf("\nThe number must be at least 1, and not more than 10");
       printf("\nPlayer #1, enter the number: ");
       scanf("%d", &choise);
-      if (choise > num[1]) {
+      if (choise > num[0]) {
         printf("\nThere are too few matches, try again");
         choise = 100;
       }
     }
-    *num += choise;
-    *(num + 1) -= choise;
-    num[2] = 2;
+    *num -= choise;
+    num[1] = 2;
 
-    game(num[1]);
-    printf("\n\n");
+    game(num[0]);
     printf("Player #2, enter the number: ");
     scanf("%d", &choise);
-
-    if (choise > num[1]) {
+    if (choise > num[0]) {
       printf("\nThere are too few matches, try again");
       choise = 100;
     }
@@ -60,15 +52,14 @@ int maingame(int *num) {
       printf("\nThe number must be at least 1, and not more than 10");
       printf("\nPlayer #2, enter the number: ");
       scanf("%d", &choise);
-      if (choise > num[1]) {
+      if (choise > num[0]) {
         printf("\nThere are too few matches, try again");
         choise = 100;
       }
     }
-    *num += choise;
-    *(num + 1) -= choise;
-    num[2] = 1;
+    *num -= choise;
+    num[1] = 1;
   }
-  int winner = num[2];
+  int winner = num[1];
   return winner;
 }
