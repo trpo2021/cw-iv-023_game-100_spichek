ef24ee7f490fae7fe54e5e9e4421b1950d5cd82b
NaicikOne <NaicikOne@yandex.ru>
2021-05-11 00:26:28 +0600

    add check function

---
+++ src/menu/check.c
@@ -0,0 +1,44 @@
+#include "check.h"
+
+int check_input(int max, char* input)
+{
+    int arr[3];
+    int i = 0;
+    int answer;
+    if (atoi(input) == 0) {
+        printf("input error. try again.\n");
+        return 0;
+    }
+    answer = atoi(input);
+    while (i < max) {
+        arr[i] = i + 1;
+        i++;
+    }
+    if (max == 1) {
+        if (answer > 1 || answer < 1) {
+            printf("input > 1 or < 1\n");
+            return 0;
+        } else
+            return answer;
+    }
+    if (max == 2) {
+        if (answer > 2 || answer < 1) {
+            printf("input > 2 or < 1\n");
+            return 0;
+        } else
+            return answer;
+    }
+    if (max == 3) {
+        if (answer > 3 || answer < 1) {
+            printf("input > 3 or < 1\n");
+            return 0;
+        } else
+            return answer;
+    }
+    if (max == 10) {
+        if (answer > 10 || answer < 1) {
+            return 0;
+        } else
+            return answer;
+    }
+}
---
+++ src/menu/check.h
@@ -0,0 +1,7 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma once
+
+int check_input(int, char*);
