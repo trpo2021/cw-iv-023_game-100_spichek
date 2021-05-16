80440813c86b7bf5e33fca71d10db878b33e9a3a
NaicikOne <NaicikOne@yandex.ru>
2021-05-16 18:05:59 +0600

    write tests (3 true, 3 false)

--- src/test/test.c
+++ src/test/test.c
@@ -1,3 +1,45 @@
 #include "ctest.h"
 #include <stdbool.h>
 #include <stdio.h>
+
+#include "menu/check.h"
+
+CTEST(input, input_in_menu_true)
+{
+    char out[2] = "1";
+    bool result;
+    int res;
+    res = check_input(1, out);
+    if (res == 1)
+        result = true;
+    else
+        result = false;
+    ASSERT_TRUE(result);
+}
+
+CTEST(input, input_name_true)
+{
+    char out[16] = "Naicik";
+    bool result = check_nick(out);
+    ASSERT_TRUE(result);
+}
+
+CTEST(input, input_in_menu_false)
+{
+    char out[2] = "a";
+    bool result;
+    int res;
+    res = check_input(1, out);
+    if (res == 1)
+        result = true;
+    else
+        result = false;
+    ASSERT_TRUE(result);
+}
+
+CTEST(input, input_name_false)
+{
+    char out[16] = "Nai12";
+    bool result = check_nick(out);
+    ASSERT_TRUE(result);
+}
